cc383cb4e8127aa05046fe56cd4cd2dec4b1b6fd
ashish patil <ash@.com>
2025-07-25 15:26:34 +0530

    Footer added

--- VRS_Frontend/src/App.jsx
+++ VRS_Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes,Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Home Page Component
 import Home from './pages/Home'
@@ -6,12 +7,18 @@ import Home from './pages/Home'
 import './App.css';
 
 
+import Footer from './components/common/Footer';
+import PrivateRoute from './components/common/PrivateRoute';
+
+
 function App() {
     return (
         <> 
+       
             <Routes>
                 <Route path="/" element = {<Home/>} />
             </Routes>
+             <Footer />
         </>
     )
 }
